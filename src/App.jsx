33dd0124a7b1be86425f3a47cced175f402c76fb
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-12-08 10:29:01 -0500

    main upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About"
+import Hero from "./components/Hero"
+import NavBar from "./components/NavBar"
+import Features from "./components/Features"
+import Story from "./components/Story"
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+    </main>
+  )
+}
+
+export default App
